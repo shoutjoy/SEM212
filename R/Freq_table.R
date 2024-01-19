4fb37c8e0d4fc37b0a7d63f22cb0c81622e0da06
USER <shoutjoy1@gmail.com>
2024-01-20 00:16:39 +0900

    Freq_table revise masking

--- R/Freq_table.R
+++ R/Freq_table.R
@@ -8,60 +8,49 @@
 #'
 #'
 #'
-Freq_table <- function(data){
-  res <-  data%>% table() %>% as.data.frame() %>%
-    mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
-    arrange(desc(Freq))%>%`colnames<-`(c("Variable","Freq","Prop"))
-  res
+
+Freq_table <- function(data,
+                       format="markdown",  #df: dataframe
+                       title="Frequency output function",
+                       sort=FALSE,
+                       prop=TRUE,
+                       arrange=""){
+  library(tidyverse)
+  library(knitr)
+
+  if(prop==TRUE){
+    if(sort==TRUE){
+      if(arrange=="desc"){
+        res <-  data%>% table() %>% as.data.frame() %>%
+          mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
+          arrange(desc(Freq)) %>%
+          `colnames<-`(c("Variable","Freq","Prop"))
+
+
+      }else if(arrange==""){
+        res <-   data%>% table() %>% as.data.frame() %>%
+          mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
+          arrange(Freq) %>%
+          `colnames<-`(c("Variable","Freq","Prop"))
+
+      }
+
+    }else if(sort==FALSE){
+      res <-     data%>% table() %>% as.data.frame() %>%
+        mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%")) %>%
+        `colnames<-`(c("Variable","Freq","Prop"))
+
+    }
+  }else if(prop==FALSE){
+    res <-  data%>% table() %>% as.data.frame()
+   }
 }
 
 
 
-# Freq_table <- function(data,
-#                        format="markdown",  #df: dataframe
-#                        title="Frequency output function",
-#                        sort=FALSE,
-#                        prop=TRUE,
-#                        arrange=""   # desc
-# ){
-#   library(tidyverse)
-#   library(knitr)
-#
-#   if(prop==TRUE){
-#     if(sort==TRUE){
-#       if(arrange=="desc"){
-#         res <-  data%>% table() %>% as.data.frame() %>%
-#           mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
-#           arrange(desc(Freq)) %>%
-#           `colnames<-`(c("Variable","Freq","Prop"))
-#
-#
-#       }else if(arrange==""){
-#         res <-   data%>% table() %>% as.data.frame() %>%
-#           mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
-#           arrange(Freq) %>%
-#           `colnames<-`(c("Variable","Freq","Prop"))
-#
-#       }
-#
-#     }else if(sort==FALSE){
-#       res <-     data%>% table() %>% as.data.frame() %>%
-#         mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%")) %>%
-#         `colnames<-`(c("Variable","Freq","Prop"))
-#
-#     }
-#   }else if(prop==FALSE){
-#     res <-  data%>% table() %>% as.data.frame()
-#   }
-#
-#
-#
-#
-#   #result summary
-#   if(format=="markdown"){
-#     res %>%
-#       kable(caption = title, format = format)
-#   }else if(format=="df"){
-#     res
-#   }
-# }
+#' Freq_table <- function(data){
+#'   res <-  data%>% table() %>% as.data.frame() %>%
+#'     mutate("Proportion(%)" = paste(round(Freq/length(data)*100,2),"%"))%>%
+#'     arrange(desc(Freq))%>%`colnames<-`(c("Variable","Freq","Prop"))
+#'   res
+#' }
