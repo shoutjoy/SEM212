b87b8a16ee5138a3ef348b1f5ddfc83cd1a7a056
USER <shoutjoy1@gmail.com>
2024-01-22 02:00:04 +0900

    lavaanplot

--- R/LavaanPlot.R
+++ R/LavaanPlot.R
@@ -6,23 +6,39 @@
 #' @param coefs TRUE
 #' @param col default 'gray60'
 #' @param covs  TRUE
-#'
+#' @param stand  std coeff
+#' @param label  input name labels=c(f1_1="exp", f1_2="want",f1_3="problem") etc
+#' @param overlap  default FALSE
+#' @param layout  'dot', 'neato', 'fdp', 'sfdp', 'circo', 'twopi', 'nop','nop2', 'osage', 'patchwork'
+#' @param digits digits = 3#'
 #' @return plot
 #' @export
 #'
 
-LavaanPlot<-function(x, stand=TRUE,
-                     rotation="LR", #option ="RL, Top
-                     coefs=TRUE,col="gray60",
-                     covs= TRUE){
+LavaanPlot <- function(x,
+                     rotation = "LR",
+                     coefs = TRUE,
+                     col = "gray60",
+                     stand = FALSE,
+                     label = NULL,
+                     covs = FALSE,
+                     stars = c("covs", "latent","regress"),
+                     overlap = FALSE,
+                     layout = "dot",
+                     digits = 3
+){
   # library(lavaanPlot)
   lavaanPlot::lavaanPlot(model = x,
-             edge_options = list(color = c(col)),
-             coefs=coefs,
-             covs= covs,
-             stars = c ("covs", "latent","regress"),
+             edge_options = list(color = c(col), digits = digits),
+             coefs = coefs,
+             covs = covs,
+             stars = stars,
+             graph_options = list(layout = layout,
+                                  rankdir = rotation,
+                                  overlap = overlap),
+             # node_options = list(shape = "box", fontname = "Helvetica"),
              stand = stand,
-             graph_options = list(layout="dot",rankdir = rotation)
+             labels = label
 
              # ,
              # graph_options = list(overlap = "true", fontsize = "10" ),
