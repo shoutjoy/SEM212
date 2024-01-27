e1c1aeb3250b73e1b4d3d36a40ffe2eef15583c0
USER <shoutjoy1@gmail.com>
2024-01-27 12:50:07 +0900

    ses_stat#19

--- R/des_stat.R
+++ R/des_stat.R
@@ -2,6 +2,7 @@
 #'
 #' @param x data.frame
 #' @param digit default 3
+#' @param type output  tpye
 #'
 #' @return reuren mean(sd) type
 #' @export
@@ -13,9 +14,15 @@
 #' mon1 = c(45, 42, 36, 39, 51, 44),
 #' mon3 = c(50, 42, 41, 35, 55, 49),
 #' mon6 = c(55, 45, 43, 40, 59, 56))
+#'
 #' df[,2:4] %>% des_stat()
+#' df[,2:4] %>% des_stat(type="all")
+#' df[,2:4] %>% des_stat(type="row")
+#' df[,2:4] %>% des_stat(type="col")
+#'
+#'
 #' }
-des_stat <- function(x, digit =3){
+des_stat <- function(x, digit =3, tpye = "col"){
   x <- x %>% as.data.frame()
   s_col <- dplyr::summarise(x,
                             dplyr::across( .cols=c(1:ncol(x)),
@@ -27,5 +34,11 @@ des_stat <- function(x, digit =3){
 
   s_row<- s_col %>% t() %>% data.frame()
 
-  return(s_col)
+  res= list(s_col, s_row)
+
+  switch(tpye,
+         all= res,
+         row = s_row,
+         col= s_col
+         )
 }
