ae5baa152ff72a377ca87d370173ee7878af12c7
USER <shoutjoy1@gmail.com>
2024-01-28 03:40:49 +0900

    lowrMat#32

--- R/lowerMat.R
+++ R/lowerMat.R
@@ -27,3 +27,34 @@ lowerMat <- function(mat) {
 
   return(mat)
 }
+
+
+#' lower pull matrix fill NA
+#'
+#' @param mat matrix data
+#' @param fill matrix data NA or .
+#'
+#' @return lowermatrix, upper 0
+#' @export
+#'
+#'
+lowerMat_na <- function(mat, fill=".") {
+  num_rows <- nrow(mat)
+  num_cols <- ncol(mat)
+
+  if (num_rows != num_cols) {
+    stop("Input matrix is not square.")
+  }
+
+  for (i in 1:num_rows) {
+    for (j in 1:num_cols) {
+      if (i == j) {
+        mat[i, j] <- fill
+      } else if (i < j) {
+        mat[i, j] <- fill
+      }
+    }
+  }
+
+  return(mat)
+}
