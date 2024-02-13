b815a1753e8364a357579df071cdd1c41177149f
USER <shoutjoy1@gmail.com>
2024-02-14 03:32:31 +0900

    ICC_pois#15

--- R/ICC_logit.R
+++ R/ICC_logit.R
@@ -9,11 +9,11 @@
 #' \dontrun{
 #' lme4::glmer(am ~ wt + (wt |vs ), mtcars,
 #'             family=binomial(link='logit'))|>
-#'             ICC.logit()
+#'             ICC_logit()
 #' }
 ICC_logit <- function(gmlm.dichotomous){
 
-  var.cov <- data.frame(VarCorr(gmlm.dichotomous))
+  var.cov <- data.frame(lme4::VarCorr(gmlm.dichotomous))
   icc2.dich <- var.cov$vcov/(sum(var.cov$vcov)+((pi^2)/3))
   var.cov$ICC = icc2.dich
   var.cov$ICC_ratio = paste0(round(icc2.dich,4) * 100,"%")
--- R/ICC_pois.R
+++ R/ICC_pois.R
@@ -12,7 +12,7 @@
 #'             ICC_pois()
 #' }
 ICC_pois <- function(gmlm.poisson){
-  var.cov <- data.frame(VarCorr(gmlm.poisson))
+  var.cov <- data.frame(lme4::VarCorr(gmlm.poisson))
   icc2.pois <- var.cov$vcov/(sum(var.cov$vcov) + 1)
   var.cov$ICC = icc2.pois
   var.cov$ICC_ratio = paste0(round(icc2.pois, 4) * 100,"%")
